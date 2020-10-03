31c3901c6e9e469a816d218c38f711ee4b292b6e
BonIlcer <pantaev.e@mail.ru>
2020-10-03 17:25:47 +0700

    Add Navbar and init Header

--- src/App.vue
+++ src/App.vue
@@ -1,21 +1,33 @@
 <template>
-  <div id="app"></div>
+  <div id="app">
+    <Navbar />
+    <Header />
+  </div>
 </template>
 
 <script>
+import Navbar from "@/components/Navbar";
+import Header from "@/components/Header";
+
 export default {
   name: "App",
-  components: {},
+  components: {
+    Navbar,
+    Header,
+  },
 };
 </script>
 
 <style>
+* {
+  box-sizing: border-box;
+  margin: 0;
+  padding: 0;
+}
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
+  font-family: "Open Sans", Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
 }
 </style>
---
+++ src/components/Header.vue
@@ -0,0 +1,11 @@
+<template>
+  <div>Header</div>
+</template>
+
+<style lang="scss" scoped>
+div {
+  border-top: 1px solid #eceff1;
+  border-bottom: 1px solid #eceff1;
+  height: 40px;
+}
+</style>
